ab9e5a5cf22acc266f1d2cb7274e7fdfa27dea74
LilianBittar <bittarlily@gmail.com>
2022-12-01 23:37:14 +0100

    Implemented mongo database connection

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContex.cs
@@ -0,0 +1,17 @@
+namespace  Catalog.API.Data;
+
+public class CatalogContext : ICatalogContext
+{
+    public IMongoCollection<Product> Products { get; }
+
+    public CatalogContext(IConfiguration configuration)
+    {
+        var client = new MongoClient(configuration["DatabaseSettings:ConnectionString"]);
+        var database = client.GetDatabase(configuration["DatabaseSettings:DatabaseName"]);
+
+        Products = database.GetCollection<Product>(configuration["DatabaseSettings:CollectionName"]);
+
+       CatalogContextSeed.SeedData(Products);
+    }
+
+}
