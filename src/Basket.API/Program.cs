73f07663026b83141dfcefcdf12e42d6604d28d5
LilianBittar <bittarlily@gmail.com>
2022-12-13 23:12:18 +0100

    connect radis docker container from basket api to docker.

--- src/Basket.API/Program.cs
+++ src/Basket.API/Program.cs
@@ -6,6 +6,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.services.AddStackExchangeRedisCche(options =>
+{
+    options.Configuration = builder.Configuration["CacheSettings:ConnectionString"];
+});
 
 var app = builder.Build();
 
