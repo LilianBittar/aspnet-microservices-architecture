40e3ca9733c6eea9f76b515472e6d3a1b63a3c9a
LilianBittar <bittarlily@gmail.com>
2023-01-18 01:55:32 +0100

    Generate Proto service from discount proto file in discount grpc.

--- src/Discount.GRPC/Program.cs
+++ src/Discount.GRPC/Program.cs
@@ -1,5 +1,5 @@
 using Discount.GRPC.Repositories;
-using Discount.GRPC.Services;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +13,7 @@ builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
