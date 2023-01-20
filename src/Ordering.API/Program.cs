9f49d6a2696c25a13d23c566beda45fc82d2f8b5
LilianBittar <bittarlily@gmail.com>
2023-01-20 16:33:26 +0100

    Implelemnting application services registration in application layer.

--- src/Ordering.API/Program.cs
+++ src/Ordering.API/Program.cs
@@ -7,6 +7,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//builder.Services.AddApplicationServices();
+//builder.Services.AddInfrastructureServices(builder.Configuration);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
