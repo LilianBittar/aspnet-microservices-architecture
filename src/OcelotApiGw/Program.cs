5a5bdbb6dc5b42c73a26bb0900264bb51d57c8de
LilianBittar <bittarlily@gmail.com>
2023-02-01 20:26:51 +0100

    Added package.

--- src/OcelotApiGw/Program.cs
+++ src/OcelotApiGw/Program.cs
@@ -1,11 +1,10 @@
-
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 builder.Logging.AddConsole();
 builder.Logging.AddDebug();
 
 var app = builder.Build();
-
 
 app.MapGet("/", () => "Hello World!");
 
