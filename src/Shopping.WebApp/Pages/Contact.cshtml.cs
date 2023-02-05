5b8d18c6aa254f35e3ca8ba494de1485e601bd73
LilianBittar <bittarlily@gmail.com>
2023-02-05 23:09:31 +0100

    Created shopping web app.

---
+++ src/Shopping.WebApp/Pages/Contact.cshtml.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Shopping.WebApp
+{
+    public class ContactModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
