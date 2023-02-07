aab36d2ab979c08376681643539f6ebe505c20b7
LilianBittar <bittarlily@gmail.com>
2023-02-07 15:10:08 +0100

    Implemented catalog service.

--- src/Shopping.WebApp/Services/CatalogService.cs
+++ src/Shopping.WebApp/Services/CatalogService.cs
@@ -1,3 +1,4 @@
+using Shopping.WebApp.Extensions;
 using Shopping.WebApp.Models;
 
 namespace Shopping.WebApp.Services;
@@ -10,24 +11,32 @@ public class CatalogService : ICatalogService
     {
         _client = client ?? throw new ArgumentException(nameof(client));
     }
-    public Task<CatalogModel> CreateCatalog(CatalogModel model)
+    public async Task<CatalogModel> CreateCatalog(CatalogModel model)
     {
-        throw new NotImplementedException();
+        var response = await _client.PostAsJson($"/Catalog", model);
+        if (response.IsSuccessStatusCode)
+            return await response.ReadContentAs<CatalogModel>();
+        else
+        {
+            throw new Exception ("Something went wrong when calling api");
+        }    
+    }
+
+    public async Task<CatalogModel> GetCatalog(string id)
+    {
+        var response = await _client.GetAsync("/Catalog/{id}");
+        return await response.ReadContentAs<CatalogModel>();
     }
 
     public async Task<IEnumerable<CatalogModel>> GetCatalog()
     {
-        var response =  await _client.GetAsync("/api/v1/Catalog");
-        //return await response.ReadContentAs<List<CatalogModel>>();
+        var response = await _client.GetAsync("/Catalog");
+        return await response.ReadContentAs<List<CatalogModel>>();
     }
 
-    public Task<CatalogModel> GetCatalog(string id)
+    public async Task<IEnumerable<CatalogModel>> GetrGatalogByCatagory(string catagory)
     {
-        throw new NotImplementedException();
-    }
-
-    public Task<IEnumerable<CatalogModel>> GetrGatalogByCatagory(string catagory)
-    {
-        throw new NotImplementedException();
+         var response = await _client.GetAsync("/Catalog/GetProductByCaatalog/{catalog}");
+        return await response.ReadContentAs<List<CatalogModel>>();
     }
 }
